99f2c2b57eaa38807afb16b3d1f7e42dc2bd10d5
Thomas Ricouard <ricouard77@gmail.com>
2021-01-06 11:04:01 +0100

    Add SQLite store

--- MortyUI/GraphQL/Network.swift
+++ MortyUI/GraphQL/Network.swift
@@ -7,8 +7,25 @@
 
 import CoreData
 import Apollo
+import ApolloSQLite
 
 class Network {
     static let shared = Network()
-    private(set) lazy var apollo = ApolloClient(url: URL(string: "https://rickandmortyapi.com/graphql")!)
+    private(set) lazy var apollo: ApolloClient = {
+        let documentsPath = NSSearchPathForDirectoriesInDomains(
+            .documentDirectory,
+            .userDomainMask,
+            true).first!
+        let documentsURL = URL(fileURLWithPath: documentsPath)
+        let sqliteFileURL = documentsURL.appendingPathComponent("db.sqlite")
+        do {
+            let sqliteCache = try SQLiteNormalizedCache(fileURL: sqliteFileURL)
+            let store = ApolloStore(cache: sqliteCache)
+            let network = RequestChainNetworkTransport(interceptorProvider: LegacyInterceptorProvider(store: store),
+                                                       endpointURL: URL(string: "https://rickandmortyapi.com/graphql")!)
+            return ApolloClient(networkTransport: network, store: store)
+        } catch {
+            return ApolloClient(url: URL(string: "https://rickandmortyapi.com/graphql")!)
+        }
+    }()
 }
